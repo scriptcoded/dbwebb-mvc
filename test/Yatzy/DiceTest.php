989047fe826916797eb20c052de7cedb5708e552
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2021-05-07 00:09:09 +0200

    Test dice

---
+++ test/Yatzy/DiceTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Manh20\Yatzy;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for the Yatzy Dice class.
+ */
+class DiceTest extends TestCase
+{
+    /**
+     * Try to create the class.
+     */
+    public function testCreateTheClass()
+    {
+        $dice = new Dice(6);
+        $this->assertInstanceOf("\Manh20\Yatzy\Dice", $dice);
+    }
+
+    /**
+     * Try to get and set the sides.
+     * If you're concerned about testing multiple things here:
+     * https://stackoverflow.com/a/5937899
+     */
+    public function testGetSetSides()
+    {
+        $dice = new Dice(6);
+        $dice->set_sides(5);
+        
+        $this->assertEquals($dice->get_sides(), 5);
+    }
+
+    /**
+     * Try to roll.
+     */
+    public function testRoll()
+    {
+        $dice = new Dice(6);
+        $res = $dice->roll();
+        
+        $this->assertEquals($res, $dice->get_last_roll());
+    }
+}
